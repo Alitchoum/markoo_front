d425cffc0501e779ff93bfe3604d758513499f24
alize <alizesuchon@gmail.com>
2026-04-24 16:17:36 +0200

    delete comments

--- markoo_front/Authentification/Service/AuthService.swift
+++ markoo_front/Authentification/Service/AuthService.swift
@@ -17,39 +17,7 @@ class AuthService{
     
     private let baseURL = "http://127.0.0.1:8080/auth"
     private var cancellables = Set<AnyCancellable>()
-    
-//    func register(firstName: String, email: String, password: String) async  {
-//        
-//        let registerRequest = RegisterRequest(firstName: firstName, email: email, password: password)
-//        
-//        guard let url = URL(string: "\(baseURL)/register") else { return }
-//        
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "POST"
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.httpBody = try? JSONEncoder().encode(registerRequest)
-//        
-//        URLSession.shared.dataTaskPublisher(for: request)
-//            .map(\.data)
-//            .decode(type: AuthResponse.self, decoder: JSONDecoder())
-//            .receive(on: DispatchQueue.main)
-//            .sink(receiveCompletion: { completion in
-//                switch completion {
-//                case .failure(let error):
-//                    print("Error registering user: \(error)")
-//                case .finished:
-//                    break
-//                }
-//            }, receiveValue: { [weak self] response in
-//                
-//                UserDefaults.standard.set(response.token, forKey: "authToken")
-//                self?.user = response.user
-//                self?.isLoggedIn = true
-//                
-//            })
-//            .store(in: &cancellables)
-//    }
-    
+        
     func register(firstName: String, email: String, password: String) async {
         let registerRequest = RegisterRequest(firstName: firstName, email: email, password: password)
         
@@ -61,10 +29,8 @@ class AuthService{
         request.httpBody = try? JSONEncoder().encode(registerRequest)
         
         do {
-            //Appel réseau avec async/await
             let (data, response) = try await URLSession.shared.data(for: request)
             
-            // 2. Vérification de la réponse HTTP
             guard let httpResponse = response as? HTTPURLResponse else {
                 await MainActor.run {
                     self.loginError = "Réponse invalide du serveur."
@@ -72,7 +38,6 @@ class AuthService{
                 return
             }
             
-           //Gestion des erreurs
             guard (200...299).contains(httpResponse.statusCode) else {
                 await MainActor.run {
                     // Vapor renvoie souvent 409 Conflict ou 400 Bad Request si l'email existe déjà
@@ -85,10 +50,8 @@ class AuthService{
                 return
             }
             
-            //Décodage du succès
             let authResponse = try JSONDecoder().decode(AuthResponse.self, from: data)
             
-            //Mise à jour de l'interface sur le MainActor
             await MainActor.run {
                 UserDefaults.standard.set(authResponse.token, forKey: "authToken")
                 self.user = authResponse.user
@@ -97,7 +60,6 @@ class AuthService{
             }
             
         } catch {
-            //En cas de perte de connexion réseau ou d'erreur de décodage JSON
             await MainActor.run {
                 self.loginError = "Erreur technique : impossible de finaliser l'inscription."
                 print("Erreur Register: \(error)")
